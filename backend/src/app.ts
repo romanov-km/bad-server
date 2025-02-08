1d83f3f5d889615ba094858843f720a46043995d
Kirill Romanov <workrkm@mail.ru>
2025-02-08 23:46:45 +0300

    feature: добавить лимитер и cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import { errors } from 'celebrate'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import ExpressMongoSanitize from 'express-mongo-sanitize'
+import rateLimit from 'express-rate-limit'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
@@ -10,17 +12,33 @@ import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
 import routes from './routes'
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 минут
+    max: 20, // Ограничение: 20 запросов за 15 минут с одного IP
+    message: 'Слишком много запросов с этого IP, попробуйте позже',
+    headers: true,
+});
+
 const { PORT = 3000 } = process.env
 const app = express()
+const allowedOrigins = 'http://localhost:5173';
 
 app.use(cookieParser())
 
-app.use(cors())
+// app.use(cors())
 // app.use(cors({ origin: ORIGIN_ALLOW, credentials: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(limiter);
+
+app.use(cors({
+    origin: allowedOrigins,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true,
+}));
 
 app.use(serveStatic(path.join(__dirname, 'public')))
 
+app.use(ExpressMongoSanitize())
 app.use(urlencoded({ extended: true }))
 app.use(json())
 
